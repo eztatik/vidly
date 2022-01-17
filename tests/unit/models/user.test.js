a087b1fcf6715ed036a0356e8556e513f315aa5f
eztatik <jromero29knights@gmail.com>
2022-01-16 22:45:15 -0500

    user test working

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -2,6 +2,7 @@
 const {User} = require('../../../Models/user');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const mongoose = require('mongoose');
 
 
 
@@ -9,10 +10,12 @@ const config = require('config');
 
 describe('user.generateAuthToken', () => {
     it('should return a valid JWT', () => {
-        const user = new User ({ _id: 1, isAdmin: true});
+        const payload = { _id: new mongoose.Types.ObjectId().toHexString(),
+             isAdmin: true};
+        const user = new User (payload);
         const token = user.generateAuthToken();
         const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
-        expect(decoded).toMatchObject({ _id: 1, isAdmin: true});
+        expect(decoded).toMatchObject(payload);
     });
 });
 
