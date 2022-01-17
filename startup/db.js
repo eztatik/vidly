01e2cbab989bfcb4b069367081bff023498935d8
eztatik <jromero29knights@gmail.com>
2022-01-17 01:14:17 -0500

    setting up the test Db

--- startup/db.js
+++ startup/db.js
@@ -1,10 +1,14 @@
 const winston  = require('winston');
 const mongoose = require('mongoose');
+const config = require('config');
 
 module.exports = function() {
     mongoose.set('useCreateIndex', true)
-    mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => winston.info('connected to mongodb'))
+    const db = config.get('db');
+    mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+    //saving this just in case. this goes in the connection path
+    //, {useNewUrlParser: true, useUnifiedTopology: true}
+    .then(() => winston.info(`connected to ${db}...`))
     //.catch(err => console.error('could not connect to Mongodb'));
 
 
